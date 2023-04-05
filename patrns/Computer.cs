9ac3eb3a9500ef1d6c79d12c1d822846b6a1a321
pavel <pavel@MSI>
2023-04-05 23:14:13 +0300

    Добавьте файлы проекта.

---
+++ patrns/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patrns
+{
+    class Computer
+    {
+        public Os Os { get; set; }
+        public void Launch(string osName)
+        {
+            Os = Os.getInstance(osName);
+        }
+    }
+}
---
+++ patrns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patrns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer comp = new Computer();
+            comp.Launch("Win8");
+            Console.WriteLine(comp.Os.Name);
+
+            comp.Os = Os.getInstance("Windows 10");
+            Console.WriteLine(comp.Os.Name);
+
+            Console.ReadLine();
+        }
+    }
+    
+}
